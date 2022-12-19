61b5313283ac1b1b64d847b99ba9dfd606ea5dbc
AntwanSherif <antwansherif@gmail.com>
2022-12-19 16:34:54 +0200

    :lipstick: Adjust home page styles

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Typography } from '@douyinfe/semi-ui';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.logoContainer}>
-        <Link href={`/`}>
-          <Image className={styles.logo} src='/logo.png' alt='pokemon logo' width='500' height='184' priority />
-        </Link>
-      </div>
-
-      <Typography.Text className={styles.total}>
-        Total:
-        <Typography.Text strong> 10</Typography.Text>
-      </Typography.Text>
+      <Link href='/'>
+        <Image className={styles.logo} src='/logo.png' alt='pokemon logo' width='500' height='184' priority />
+      </Link>
     </div>
   );
 }
