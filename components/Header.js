29d67fb6959a3b29a7df0847cfd388dc0f0985aa
AntwanSherif <antwansherif@gmail.com>
2022-12-19 13:07:44 +0200

    :children_crossing: Use the logo for navigation

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import Image from 'next/image';
-import { Layout, Typography } from '@douyinfe/semi-ui';
+import { Typography } from '@douyinfe/semi-ui';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <Image className={styles.logo} src='/logo.png' alt='pokemon logo' width='500' height='184' priority />
+        <Link href={`/`}>
+          <Image className={styles.logo} src='/logo.png' alt='pokemon logo' width='500' height='184' priority />
+        </Link>
       </div>
 
       <Typography.Text>
