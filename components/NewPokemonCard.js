d90f6007d9abb316021048bc679869f150963183
AntwanSherif <antwansherif@gmail.com>
2022-12-19 17:21:44 +0200

    :children_crossing: Show loading indicator while creating new pokemon

--- components/NewPokemonCard.js
+++ components/NewPokemonCard.js
@@ -1,28 +1,46 @@
-import { Card } from '@douyinfe/semi-ui';
+import { useState } from 'react';
+import Image from 'next/image';
+import { Card, Spin } from '@douyinfe/semi-ui';
 import { IconPlus } from '@douyinfe/semi-icons';
 import styles from './NewPokemonCard.module.css';
 
 const newPokemonsNames = ['karma', 'Maged', 'Meego', 'Aya', 'Samir'];
 
 export function NewPokemonCard() {
-  const createNewPokemon = () =>
+  const [loading, setLoading] = useState(false);
+
+  const createNewPokemon = () => {
+    setLoading(true);
+
     fetch(`/api/pokemons`, {
       method: 'POST',
       body: newPokemonsNames.pop()
+    }).finally(() => {
+      setLoading(false);
     });
+  };
+
+  const cardClasses = loading ? `${styles.card} ${styles.loading}` : styles.card;
+  const contentClasses = loading ? `${styles.content} ${styles.loading}` : styles.content;
 
   return (
     <Card
       shadows='hover'
-      className={styles.card}
+      className={cardClasses}
       cover={
-        <div className={styles.content}>
-          <IconPlus className={styles.icon} style={{ color: 'white' }} />
+        <div className={contentClasses}>
+          {loading ? (
+            <Image src='/creating.gif' alt='creating new pokemon' width={130} height={30} />
+          ) : (
+            <IconPlus className={styles.icon} style={{ color: 'white' }} />
+          )}
         </div>
       }
-      onClick={createNewPokemon}
+      onClick={!loading ? createNewPokemon : undefined}
     >
-      <Card.Meta title='Surprise me!' className={styles.text} />
+      <div className={styles.cardBody}>
+        {loading ? <Spin /> : <Card.Meta title='Surprise me!' className={styles.text} />}
+      </div>
     </Card>
   );
 }
