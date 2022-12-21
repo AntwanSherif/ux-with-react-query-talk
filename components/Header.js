6ff5cbcf9940239a4fd343c1f07468decc46b2a4
AntwanSherif <antwansherif@gmail.com>
2022-12-21 15:43:40 +0200

    :children_crossing: Global loading indicator

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,23 @@
+import { useIsFetching } from '@tanstack/react-query';
 import Link from 'next/link';
 import Image from 'next/image';
+import { IconCheckboxTick, IconSpin } from '@douyinfe/semi-icons';
 import styles from './Header.module.css';
 
 export function Header() {
+  const isFetching = useIsFetching();
+
   return (
     <div className={styles.container}>
       <Link href='/pokemons'>
         <Image className={styles.logo} src='/logo.png' alt='pokemon logo' width='500' height='184' priority />
       </Link>
+
+      {Boolean(isFetching) ? (
+        <IconSpin spin style={{ color: 'white', fontSize: '1rem' }} />
+      ) : (
+        <IconCheckboxTick style={{ color: 'white', fontSize: '1.3rem' }} />
+      )}
     </div>
   );
 }
