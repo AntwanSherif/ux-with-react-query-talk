b918a7ef7e1d625327287eeb9ffa5f9543576883
AntwanSherif <antwansherif@gmail.com>
2022-12-21 15:18:23 +0200

    :children_crossing: Disable refetch on window focus

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@styles/globals.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  refetchOnWindowFocus: false
+});
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page);
